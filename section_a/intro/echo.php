b8bc5e56cb5c0821ab562e6ed084c4a718fd8887
Joffrey LUDOVICI <ludovici.joffrey@gmail.com>
2024-01-10 11:46:15 +0100

    complete page 33

--- section_a/intro/echo.php
+++ section_a/intro/echo.php
@@ -1,3 +1,11 @@
+<?php
+/*
+This screen display the name of the current member's name 
+and details of the current offer 
+*/
+$name = "Joffrey";
+$price = 2; 
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,7 +14,10 @@
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2><?php echo 'Joffrey\'s'; ?> page</h2>
-    <?php echo '<p class="offer">Offer: 20% off</p>' ?>
+    <h2><?php echo $name; //display greetings with name?>'s page</h2>
+    <?php echo '<p class="offer">Offer: 20% off</p>' #Display reduction's offer ?>
+    <p>
+      The cost of your candy pack is $<?php echo $price; ?> per pack.
+    </p>
   </body>
 </html>
